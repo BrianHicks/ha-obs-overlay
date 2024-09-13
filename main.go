8a5e98a039a1df212c60230b7b3f4742cdfca44b
Brian Hicks <brian@brianthicks.com>
2024-09-13 06:52:11 -0500

    add proxies for the data I want

--- main.go
+++ main.go
@@ -2,9 +2,42 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
 )
+
+func proxyHomeAssistantState(state string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		url := fmt.Sprintf("http://homeassistant.local:8123/api/states/%s", state)
+
+		// Create a new HTTP request
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatalf("Error creating request: %v", err)
+		}
+
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("HA_TOKEN")))
+		req.Header.Set("Content-Type", "application/json")
+
+		// Create an HTTP client and send the request
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatalf("Error making request: %v", err)
+		}
+		defer resp.Body.Close()
+
+		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+
+		// Read the response body
+		_, err = io.Copy(w, resp.Body)
+		if err != nil {
+			http.Error(w, "Error copying HA response", http.StatusInternalServerError)
+		}
+	}
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +48,9 @@ func main() {
 		}
 		w.Write(html)
 	})
+
+	http.HandleFunc("/state/steps", proxyHomeAssistantState("sensor.brians_iphone_steps"))
+	http.HandleFunc("/state/co2", proxyHomeAssistantState("sensor.aranet4_1eef2_carbon_dioxide"))
 
 	err := http.ListenAndServe(":9999", nil)
 	if err != nil {
